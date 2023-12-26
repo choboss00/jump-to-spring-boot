9701cb63275d35bc7c65a34efc2981f8b0100fe6
Jin_00 <anjdal64@gmail.com>
2023-12-26 20:44:20 +0900

    test : JPA 동작 테스트
    Signed-off-by: Jin_00 <anjdal64@gmail.com>

--- sbb/src/test/java/com/example/sbb/board/BoardTests.java
+++ sbb/src/test/java/com/example/sbb/board/BoardTests.java
@@ -1,2 +1,33 @@
-package com.example.sbb.board;public class BoardTests {
+package com.example.sbb.board;
+
+import com.example.sbb.Board.Board;
+import com.example.sbb.Board.BoardRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class BoardTests {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    @DisplayName("JPA 동작 테스트")
+    void testJpa() {
+        // given
+        Board board = Board.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+        // when
+        boardRepository.save(board);
+
+        // then
+        Board b1 = boardRepository.findById(1).orElseThrow(RuntimeException::new);
+
+        assertThat(b1.getTitle()).isEqualTo("제목");
+    }
 }
