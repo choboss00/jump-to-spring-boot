bb0e61e1534721787d1d8b41e58502188b011f1c
Jin_00 <anjdal64@gmail.com>
2023-12-26 20:19:07 +0900

    feat : 공통 필드를 하나로 묶는 추상 클래스 생성
    Signed-off-by: Jin_00 <anjdal64@gmail.com>

--- sbb/src/main/java/com/example/sbb/config/BaseTime.java
+++ sbb/src/main/java/com/example/sbb/config/BaseTime.java
@@ -1,2 +1,31 @@
-package com.example.sbb.config;public class BaseTime {
+package com.example.sbb.config;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTime {
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    @ColumnDefault("false")
+    private boolean isDeleted;
 }
