c281db8a5ea90b588075266061f460594fad4041
Jin_00 <anjdal64@gmail.com>
2024-01-05 15:08:04 +0900

    test : 회원가입 테스트
    Signed-off-by: Jin_00 <anjdal64@gmail.com>

--- sbb/src/test/java/com/example/sbb/user/UserTests.java
+++ sbb/src/test/java/com/example/sbb/user/UserTests.java
@@ -1,4 +1,39 @@
 package com.example.sbb.user;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 public class UserTests {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("회원가입 로직 테스트")
+    void CreateUserTests() {
+        // given
+        UserRequest.UserCreateDTO userCreateDTO = new UserRequest.UserCreateDTO();
+
+        userCreateDTO.setUsername("test");
+        userCreateDTO.setEmail("test@example.com");
+        userCreateDTO.setPassword("test1234");
+        userCreateDTO.setNickname("testst");
+        userCreateDTO.setRole(Role.USER);
+        // when
+        userService.signup(userCreateDTO);
+
+        // then
+        User user = userRepository.findByEmail("test@example.com");
+
+        assertEquals(user.getName(), "test");
+
+    }
+
 }
