848407290dc681b982e24753f2d23b1c93c3dd87
Jin_00 <anjdal64@gmail.com>
2024-01-05 16:04:46 +0900

    feat : 공통 응답 클래스 생성
    Signed-off-by: Jin_00 <anjdal64@gmail.com>

--- sbb/src/main/java/com/example/sbb/config/utils/ApiResponseBuilder.java
+++ sbb/src/main/java/com/example/sbb/config/utils/ApiResponseBuilder.java
@@ -1,4 +1,39 @@
 package com.example.sbb.config.utils;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+public class ApiResponseBuilder {
+
+    private static final String SUCCESS_STATUS = "success";
+    private static final String FAIL_STATUS = "fail";
+    private static final String ERROR_STATUS = "error";
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class ApiResponse<T> {
+        private final String status;
+        private final T data;
+        private final String message;
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(SUCCESS_STATUS, data, null);
+    }
+
+    public static <T> ApiResponse<T> successWithNoContent() {
+        return new ApiResponse<>(SUCCESS_STATUS, null, null);
+    }
+
+    public static ApiResponse<?> fail(Map<String, String> errors, String message) {
+        return new ApiResponse<>(FAIL_STATUS, errors, message);
+    }
+
+    public static ApiResponse<?> error(String message) {
+        return new ApiResponse<>(ERROR_STATUS, null, message);
+    }
 }
